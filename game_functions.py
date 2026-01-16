a96bb695451c509265f448c8dc8796f66ca3387d
MarcusD201 <mlmd201@exeter.ac.uk>
2026-01-16 13:14:24 +0000

    Modified hangman function to function properly

--- game_functions.py
+++ game_functions.py
@@ -17,4 +17,12 @@ def check_higher_lower(current_val, next_val, user_input):
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
-    pass
+    word_length= len(word)
+    word_list = list(word)
+    for i in range(word_length) :
+        if letter == word_list[i]:
+            board[i] = letter
+    if letter in board:
+        print("The letter " + letter+" is in the word")
+    else:
+        print("The letter " + letter+" is not in the word")
